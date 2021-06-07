98aa734a307f8a5fd457df74f70be998da95643a
grubeng <61166458+grubeng@users.noreply.github.com>
2021-06-07 12:05:38 +0200

    Implements creado

--- BACK/src/main/java/com/trobify/trobify/clases/InmuebleFabrica/CreadorInmueble.java
+++ BACK/src/main/java/com/trobify/trobify/clases/InmuebleFabrica/CreadorInmueble.java
@@ -2,7 +2,7 @@ package com.trobify.trobify.clases.InmuebleFabrica;
 
 import com.trobify.trobify.dto.InmuebleDTO;
 
-public class CreadorInmueble {
+public class CreadorInmueble implements Creador {
 
     public static Inmueble crearInmueble(String tipo, InmuebleDTO inmuebleDTO) {
         if(tipo.equals("venta")){
